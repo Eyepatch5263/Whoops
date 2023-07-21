096c7f0e8b624173703ad54727eac8dc187a0744
Eyepatch <pratyushpragyey@gmail.com>
2023-07-22 02:12:37 +0530

    step-19

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class CloudStorageException implements Exception {
+  const CloudStorageException();
+}
+//C in CRUD
+class CloudNotCreateNoteException extends CloudStorageException{}
+
+//R in CRUD
+class CloudNotGetAllNotesException extends CloudStorageException{}
+
+//U in CRUD
+class CloudNotUpdateNoteException extends CloudStorageException{}
+
+//D in CRUD
+class CloudNotDeleteNoteException extends CloudStorageException{}
+
+
+
+
