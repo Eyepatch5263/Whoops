11250b3116d8d97bfb88f07045462ae0aeb56204
Eyepatch <pratyushpragyey@gmail.com>
2023-07-21 23:00:11 +0530

    step-18

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:whoops4/Extensions/list/filter.dart';
 import 'package:whoops4/services/crud/notes_services.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
@@ -27,9 +28,15 @@ class NotesService {
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
-
-  
+  Stream<List<DatabaseNote>> get allNotes =>
+      _notesStreamController.stream.filter((note) {
+        final currentUser = _user;
+        if (currentUser != null) {
+          return note.userId == currentUser.id;
+        } else {
+          throw UserShouldBeSetBeforeReadingAllNotes();
+        }
+      });
 
   Future<DatabaseUser> getOrCreateUser({
     required String email,
